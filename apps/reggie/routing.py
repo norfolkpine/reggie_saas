f0f7d838f57726fa802a81419df209068a51b1e6
cleverhuman <cleverhuman110@gmail.com>
2025-09-19 02:51:40 +0900

    create vault agent

--- apps/reggie/routing.py
+++ apps/reggie/routing.py
@@ -2,11 +2,14 @@ from channels.auth import AuthMiddlewareStack
 from django.urls import re_path
 
 from .consumers import StreamAgentConsumer
+from .vault_consumers import VaultStreamConsumer
 
 # HTTP routes handled by channels
 http_urlpatterns = [
     # Stream agent endpoint
     re_path(r"^reggie/api/v1/chat/stream/", AuthMiddlewareStack(StreamAgentConsumer.as_asgi())),
+    # Vault chat endpoint
+    re_path(r"^reggie/api/v1/vault/chat/stream/", AuthMiddlewareStack(VaultStreamConsumer.as_asgi())),
 ]
 
 # Websocket routes
