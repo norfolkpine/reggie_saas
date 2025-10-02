487525e03e6503d5970a790237ddd0581cedaa6b
cleverhuman <cleverhuman110@gmail.com>
2025-10-01 22:22:39 -0700

    import stripe model

--- apps/subscriptions/forms.py
+++ apps/subscriptions/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
-from djstripe.models import SubscriptionItem, UsageRecord
+from djstripe.models.billing import SubscriptionItem, UsageRecord
 
 from apps.subscriptions.models import SubscriptionModelBase
 
