7b8e4c61d62fc23c101e9b640d14c0685a5460e6
norfolkpine <51038209+norfolkpine@users.noreply.github.com>
2025-10-02 23:06:39 +0800

    Update Docker group user in deployment script to ubuntu
    - Changed the user added to the Docker group from debian to ubuntu for accuracy in the deployment workflow.
    - This adjustment ensures that the correct user has the necessary permissions for Docker operations.

--- infra/envs/prod/main.tf
+++ infra/envs/prod/main.tf
@@ -257,8 +257,8 @@ resource "google_compute_instance" "opie_stack_vm" {
     curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
     
-    # Add debian user to docker group
-    usermod -aG docker debian
+    # Add ubuntu user to docker group
+    usermod -aG docker ubuntu
     
     # Enable Docker service
     systemctl enable docker
